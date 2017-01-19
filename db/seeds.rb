06052f6f740c8f5e271098fc3fc55be36e7e0312
Nathan Birrell <nathanbirrell@gmail.com>
2017-01-19 15:11:13 +1100

    Add Bells as seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
+
+spot_bells = Spot.find_or_initialize_by(name: 'Bells Beach')
+spot_bells.description = 'Long right-hand point'
+spot_bells.optimal_wind_direction = 'NNW'
+spot_bells.optimal_swell_direction = 'SW'
+spot_bells.season = 'March'
+spot_bells.lat = -38.3699279
+spot_bells.lng = 144.2779936
+spot_bells.save!
+puts 'CREATED SPOT: ' << spot_bells.name
