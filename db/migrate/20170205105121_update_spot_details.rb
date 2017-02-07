f0506ad874fb5e3360a7fa5b20d7d3aaa703e94f
Nathan Birrell <nathanbirrell@gmail.com>
2017-02-07 15:24:35 +1100

    Fix rename migration to delete and create instead of actual rename

--- db/migrate/20170205105121_update_spot_details.rb
+++ db/migrate/20170205105121_update_spot_details.rb
@@ -1,9 +1,9 @@
 class UpdateSpotDetails < ActiveRecord::Migration[5.0]
   def change
-    change_column :spots, :optimal_wind_direction, :integer
-    change_column :spots, :optimal_swell_direction, :integer
+    add_column :spots, :optimal_wind_direction_degrees, :integer
+    add_column :spots, :optimal_swell_direction_degrees, :integer
 
-    rename_column :spots, :optimal_wind_direction, :optimal_wind_direction_degrees
-    rename_column :spots, :optimal_swell_direction, :optimal_swell_direction_degrees
+    remove_column :spots, :optimal_wind_direction
+    remove_column :spots, :optimal_swell_direction
   end
 end
