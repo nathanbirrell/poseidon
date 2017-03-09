f664b37cc0a9a314e3faf5627a485fd1701e048e
Nathan Birrell <nathanbirrell@gmail.com>
2017-03-09 14:49:17 +1100

    Add better seeds for local build

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,14 +9,24 @@
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
 
-# spot_bells = Spot.find_or_initialize_by(name: 'Bells Beach')
-# spot_bells.description = 'Long right-hand point'
-# spot_bells.optimal_wind_direction_degrees = '350'
-# spot_bells.optimal_swell_direction_degrees = '180'
-# spot_bells.season = 'March'
-# spot_bells.image = 'http://www.surfcoast.vic.gov.au/files/assets/public/tourism/images/bellsbeachlandscape.jpg'
-# spot_bells.latitude = -38.3699279
-# spot_bells.longitude = 144.2779936
-# spot_bells.region = 144.2779936
-# spot_bells.save!
-# puts 'CREATED SPOT: ' << spot_bells.name
+region = Region.create(
+    name: 'Mornington Peninsula',
+    description: 'East of Melbourne.',
+    country: 'Australia',
+    state: 'Victoria'
+  )
+
+Spot.create(
+  name: 'Cape Schanck',
+  description: 'Long left-hand point',
+  optimal_wind_direction_degrees: '0',
+  optimal_swell_direction_degrees: 200,
+  optimal_tide_height_metres: 1.2,
+  optimal_tide_height_low_metres: 1,
+  optimal_tide_height_high_metres: 1.5,
+  season: 'March',
+  image: 'http://www.surfcoast.vic.gov.au/files/assets/public/tourism/images/bellsbeachlandscape.jpg',
+  latitude: -38.3699279,
+  longitude: 144.2779936,
+  region_id: region.id
+)
