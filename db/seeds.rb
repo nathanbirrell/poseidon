8e8eba357c095a588d7199ed1f638e063d85052c
Nathan Birrell <nathanbirrell@gmail.com>
2017-03-15 08:49:23 +1100

    Improve seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,26 +7,34 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 user = CreateAdminService.new.call
-puts 'CREATED ADMIN USER: ' << user.email
+puts 'Created Admin: ' << user.email
 
 region = Region.create(
-    name: 'Mornington Peninsula',
-    description: 'East of Melbourne.',
-    country: 'Australia',
-    state: 'Victoria'
-  )
+  name: 'Mornington Peninsula',
+  description: 'East of Melbourne.',
+  country: 'Australia',
+  state: 'Victoria'
+)
+puts "Created Region #{region.name}"
 
-Spot.create(
+spot = Spot.create(
   name: 'Cape Schanck',
   description: 'Long left-hand point',
-  wind_optimal_direction_min_degrees: '0',
-  swell_optimal_direction_min_degrees: 200,
-  tide_optimal_min_metres: 1.2,
-  tide_optimal_min_metres: 1,
-  tide_optimal_max_metres: 1.5,
   season: 'March',
   image: 'http://www.surfcoast.vic.gov.au/files/assets/public/tourism/images/bellsbeachlandscape.jpg',
-  latitude: -38.3699279,
-  longitude: 144.2779936,
-  region_id: region.id
+  latitude: -38.489189,
+  longitude: 144.884256,
+  region_id: region.id,
+  tide_optimal_min_metres: 0.6,
+  tide_optimal_max_metres: 1.3,
+  swell_optimal_size_min_metres: 1.5,
+  swell_optimal_size_max_metres: 4,
+  swell_optimal_period_seconds: 18,
+  swell_optimal_direction_min_degrees: 70,
+  swell_optimal_direction_max_degrees: 120,
+  wind_optimal_strength_min_kmh: 0,
+  wind_optimal_strength_max_kmh: 30,
+  wind_optimal_direction_min_degrees: 0,
+  wind_optimal_direction_max_degrees: 120
 )
+puts "Created Spot #{spot.name}"
