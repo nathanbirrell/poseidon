c8aa9fc7b688ae377ca6e7659d5d1a6dfe6fde87
Nathan Birrell <nathanbirrell@gmail.com>
2017-03-19 20:10:25 +1100

    Tidy up spot model

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -4,20 +4,4 @@ class Spot < ApplicationRecord
   belongs_to :region
 
   validates :name, presence: true
-
-  def forecasted_tide
-    0.5
-  end
-
-  def tide_rating
-    rating = 0
-
-    if forecasted_tide.between?(tide_optimal_min_metres, tide_optimal_max_metres)
-      rating = 10;
-    else
-      rating = 1;
-    end
-
-    rating
-  end
 end
