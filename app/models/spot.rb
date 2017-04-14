6b703b8d249d1b19dd7270033ab124c5f19df80e
Nathan Birrell <nathanbirrell@gmail.com>
2017-04-14 13:10:11 +1000

    Start rating calculation with tide only

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -7,12 +7,37 @@ class Spot < ApplicationRecord
 
   validates :name, presence: true
 
+  # get latest latest_observation data
+  def latest_observation
+    Observation.where(spot_id: id).first
+  end
+
+  # caclulate a tide rating
+  def tide_rating
+    return 0 unless latest_observation
+
+    rating = 0
+
+    tide_within_optimal_params = (latest_observation.tide_height_metres > tide_optimal_min_metres) || (latest_observation.tide_height_metres < tide_optimal_max_metres)
+
+    rating = 1 if tide_within_optimal_params
+
+    rating
+  end
+
   def current_potential
-    # get latest observation data
-    # caclulate a tide rating
+    # calculate aggregate potential rating based on tide/wind/swell
+
+    aggregate = tide_rating / 3
+
     # calculate a wind rating
     # calculate a swell rating
     # calculate an aggregate rating (as %)
-    0
+
+    aggregate
   end
+
+  private
+
+  # private methods here
 end
