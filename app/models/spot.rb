779c10507a5f6290a3b5e082291b06c22fd8fe46
Nathan Birrell <nathanbirrell@gmail.com>
2017-04-14 14:23:23 +1000

    Start basic optimal wind rating algorithm

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -16,28 +16,53 @@ class Spot < ApplicationRecord
   def tide_rating
     return 0 unless latest_observation
 
-    rating = 0
+    rating = 0.0
 
-    tide_within_optimal_params = (latest_observation.tide_height_metres > tide_optimal_min_metres) || (latest_observation.tide_height_metres < tide_optimal_max_metres)
+    is_optimal_tide = is_inside_optimal_params(latest_observation.tide_height_metres, tide_optimal_min_metres, tide_optimal_max_metres)
+    rating += 1 if is_optimal_tide
 
-    rating = 1 if tide_within_optimal_params
-
+    puts("tide_rating: #{rating.to_s}")
     rating
   end
 
-  def current_potential
-    # calculate aggregate potential rating based on tide/wind/swell
+  # calculate a wind rating
+  def wind_rating
+    return 0 unless latest_observation
 
-    aggregate = tide_rating / 3.0
+    weight_of_optimal_wind_speed = 0.2
+    weight_of_optimal_wind_direction = 0.8
 
-    # calculate a wind rating
+    rating = 0.0
+
+    is_optimal_wind_speed = is_inside_optimal_params(latest_observation.wind_strength_kmh, wind_optimal_strength_min_kmh, wind_optimal_strength_max_kmh)
+    rating += weight_of_optimal_wind_speed if is_optimal_wind_speed
+
+    is_optimal_wind_direction = is_inside_optimal_params(latest_observation.wind_direction_degrees, wind_optimal_direction_min_degrees, wind_optimal_direction_max_degrees)
+    rating += weight_of_optimal_wind_direction if is_optimal_wind_direction
+
+    puts("wind_rating: #{rating.to_s}")
+    rating
+  end
+
+  def swell_rating
     # calculate a swell rating
-    # calculate an aggregate rating (as %)
+    1
+  end
+
+  def current_potential
+    # calculate aggregate potential rating based on tide/wind/swell (as a percentage)
+    aggregate = 0.0
+
+    aggregate += tide_rating / 3.0
+    aggregate += wind_rating / 3.0
+    aggregate += swell_rating / 3.0
 
     aggregate * 100.0
   end
 
   private
 
-  # private methods here
+  def is_inside_optimal_params(observation, param_min, param_max)
+    (observation > param_min) || (observation < param_max)
+  end
 end
