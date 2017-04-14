0254681a5a9d379befe3320d555af42f0da6e036
Nathan Birrell <nathanbirrell@gmail.com>
2017-04-14 12:14:39 +1000

    Write out simple logic for current_potential calculation

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -3,5 +3,16 @@ class Spot < ApplicationRecord
 
   belongs_to :region
 
+  has_many :observations
+
   validates :name, presence: true
+
+  def current_potential
+    # get latest observation data
+    # caclulate a tide rating
+    # calculate a wind rating
+    # calculate a swell rating
+    # calculate an aggregate rating (as %)
+    0
+  end
 end
