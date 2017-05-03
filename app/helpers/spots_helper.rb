e98f6abefa68a0ff56fb2cbe9eb235d21fbbe521
Nathan Birrell <nathanbirrell@gmail.com>
2017-05-03 15:53:50 +1000

    Move Observation table into Spot view helper

--- app/helpers/spots_helper.rb
+++ app/helpers/spots_helper.rb
@@ -1,2 +1,34 @@
 module SpotsHelper
+  def spot_observations_table(spot)
+    return '' unless spot.observations
+
+    date_attrs = ['created_at', 'updated_at', 'axes_reftime', 'axes_time']
+
+    table = ''
+    table += '<table>'
+    table += '<thead><tr>'
+
+    @spot.observations.columns.map do |column|
+      table += "<td>#{column.name}</td>"
+    end
+
+    table += '</tr></thead>'
+
+    table += '<tbody>'
+
+    @spot.observations.each do |observation|
+      table += '<tr>'
+      observation.attributes.each do |attr_name, attr_value|
+        attr_value = attr_value.localtime.strftime("%a, %e %b %Y %H:%M") if date_attrs.include? attr_name
+        table += "<td>#{attr_value}</td>"
+      end
+      table += '</tr>'
+    end
+
+    table += '</tbody>'
+
+    table += '</table>'
+
+    table.html_safe
+  end
 end
