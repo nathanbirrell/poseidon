a4482e8ff44ff759ab11ac6eee282e80f5b8d8d6
Nathan Birrell <nathanbirrell@gmail.com>
2017-05-05 11:54:15 +1000

    Better observation view for spots

--- app/helpers/spots_helper.rb
+++ app/helpers/spots_helper.rb
@@ -2,6 +2,7 @@ module SpotsHelper
   def spot_observations_table(spot)
     return '' unless spot.observations
 
+    visible_attrs = ['swell_size_metres', 'swell_period_seconds', 'swell_direction_degrees', 'axes_time']
     date_attrs = ['created_at', 'updated_at', 'axes_reftime', 'axes_time']
 
     table = ''
@@ -9,6 +10,7 @@ module SpotsHelper
     table += '<thead><tr>'
 
     @spot.observations.columns.map do |column|
+      next unless visible_attrs.include? column.name
       table += "<td>#{column.name}</td>"
     end
 
@@ -19,6 +21,7 @@ module SpotsHelper
     @spot.observations.each do |observation|
       table += '<tr>'
       observation.attributes.each do |attr_name, attr_value|
+        next unless visible_attrs.include? attr_name
         attr_value = attr_value.localtime.strftime("%a, %e %b %Y %H:%M") if date_attrs.include? attr_name
         table += "<td>#{attr_value}</td>"
       end
