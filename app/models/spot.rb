244fa0ea4ed887e5b4f0447fb40eef8fc51df562
TaylorBett <taylor.bett@hotmail.com>
2017-05-09 16:33:27 +1000

    Added tide calculation + some display updates for showing data

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -54,46 +54,29 @@ class Spot < ApplicationRecord
     Tide.next_tide(id)
   end
 
-  def tidal_range
-    if last_tide.tide_type == 'low'
-      low_tide = last_tide
-      high_tide = next_tide
-    elsif  last_tide.tide_type == 'high'
-      low_tide = next_tide
-      high_tide = last_tide
-    end
-    tide_range = high_tide.height - low_tide.height
+  def low_tide
+    return last_tide.height < next_tide.height ? last_tide : next_tide
   end
 
-  # calculate current tide
-  # TODO -- where do we put this code?
-  def current_tide
-    if last_tide.tide_type == 'low'
-      low_tide = last_tide
-      high_tide = next_tide
-    elsif  last_tide.tide_type == 'high'
-      low_tide = next_tide
-      high_tide = last_tide
-    end
-    # get tide range at location
-    # get baseline (minimum) time for tide at location
+  def high_tide
+    return last_tide.height > next_tide.height ? last_tide : next_tide
+  end
 
-    # calculate difference from baseline time to current time (or target time) in multiples of 12, get modulus
-    # if modulus < 6, it is rising (add to min), if > 6, subtract from max
-    # use rule of 12ths to calc value to add/subtract. (no of 12ths x Maximum)
+  def tidal_range
+    return high_tide.height - low_tide.height
+  end
 
-    # SINE CURVE
-    # y = a.sin(b(x + l)) + v
-    # where a = amplitude, b = period, l = left shift, v = vertical shift
-
-    curr_tide = 0.0
+  def current_tide_height
     if last_tide.tide_type == 'low'
       curr_tide = tidal_range*sin(PI*1/6 - PI/2) + (tidal_range/2) + low_tide.height
     elsif last_tide.tide_type == 'high'
-      curr_tide = tidal_range*sin(PI*1/6 + PI/2) + (tidal_range/2)
-      #need to add + (last_tide.height - tide_range) instead of just + last_tide.height
+      curr_tide = tidal_range*sin(PI*1/6 + PI/2) + (tidal_range/2) + low_tide.height
     end
     return curr_tide.round(2)
+  end
+
+  def current_tide_rating
+    return 1.0
   end
 
   def current_potential
