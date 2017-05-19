6d6a0c9d7bc35265fada83eded70bd29f141ed8e
TaylorBett <taylor.bett@hotmail.com>
2017-05-19 14:51:47 +1000

    F33 | Add parabolic rating calc to Swell size in swell model

--- app/models/swell.rb
+++ app/models/swell.rb
@@ -17,19 +17,30 @@ class Swell < WeatherForecast
 
   def rating
     return 0 unless size && direction && period
-
     weight_of_optimal_swell_height = 0.7
     weight_of_optimal_swell_direction = 0.3
 
-    rating = 0.0
+    #========= CALC SWELL SIZE RATING ==========
+    # use vertex quad formula y = a(x-h)^2 + k
+    # where a = stretch coefficient, h = x coord of vertex, k = y coord of vertex
+    kVar = 100.0
+    max = spot.swell_optimal_size_max_metres
+    min = spot.swell_optimal_size_min_metres
+    hVar = ((max - min)/2) + min
 
-    is_optimal_swell_height = is_between(size, spot.swell_optimal_size_min_metres, spot.swell_optimal_size_max_metres)
-    rating += weight_of_optimal_swell_height if is_optimal_swell_height
+    # pass in known coord to determin var a value, (min, 75)
+    aVar = (75 - 100)/((min - hVar)**2)
+
+    rating = aVar * ((size - hVar)**2) + kVar
+
+    #puts("Parabolic min=#{min} max=#{max} direction=#{direction}")
+    puts("Parabolic aVar=#{aVar} hVar=#{hVar} rating=#{rating}")
+
 
     is_optimal_swell_direction = is_angle_inside_range(direction, spot.swell_optimal_direction_min_degrees, spot.swell_optimal_direction_max_degrees)
     rating += weight_of_optimal_swell_direction if is_optimal_swell_direction
 
     puts("swell_rating: #{rating.to_s}")
-    rating
+    rating.round(2)
   end
 end
