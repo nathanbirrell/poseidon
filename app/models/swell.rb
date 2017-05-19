659251d505c0e9efe295899110624a6860d2291a
TaylorBett <taylor.bett@hotmail.com>
2017-05-19 14:59:29 +1000

    F33 | Added skeleton of swell direction rating ready for when db is updated to provide optimum and max variance for swell direction (like wind)

--- app/models/swell.rb
+++ app/models/swell.rb
@@ -31,15 +31,28 @@ class Swell < WeatherForecast
     # pass in known coord to determin var a value, (min, 75)
     aVar = (75 - 100)/((min - hVar)**2)
 
-    rating = aVar * ((size - hVar)**2) + kVar
+    sizeRating = aVar * ((size - hVar)**2) + kVar
 
     #puts("Parabolic min=#{min} max=#{max} direction=#{direction}")
-    puts("Parabolic aVar=#{aVar} hVar=#{hVar} rating=#{rating}")
+    puts("Parabolic aVar=#{aVar} hVar=#{hVar} sizeRating=#{sizeRating}")
 
+    #========= CALC SWELL DIRECTION RATING ==========
+    # use vertex quad formula y = a(x-h)^2 + k
+    # where a = stretch coefficient, h = x coord of vertex, k = y coord of vertex
+    dirMaxVariance = 25.0 #Need to get this from DB or calc
+    dirKVar = 100.0
+    # hVar = ((max - min)/2) + min
+    dirHVar = 0.0
 
-    is_optimal_swell_direction = is_angle_inside_range(direction, spot.swell_optimal_direction_min_degrees, spot.swell_optimal_direction_max_degrees)
-    rating += weight_of_optimal_swell_direction if is_optimal_swell_direction
+    # pass in known coord to determin var a value, (dirMaxVariance, 75)
+    dirAVar = (75 - 100)/((dirMaxVariance - dirHVar)**2)
 
+    dirCurrentVariance = 35.0 #need to calc, variance of direction compared to optimal wind dir
+    dirRating = dirAVar * ((dirCurrentVariance - dirHVar)**2) + dirKVar
+
+    puts("Parabolic dirAVar=#{dirAVar} dirHVar=#{dirHVar} dirRating=#{dirRating}")
+
+    rating = sizeRating
     puts("swell_rating: #{rating.to_s}")
     rating.round(2)
   end
