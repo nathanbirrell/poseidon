b9d835ac04ed7b9671baa46e584b84a9a7743b12
Nathan Birrell <nathanbirrell@gmail.com>
2017-07-28 16:32:07 +1000

    F78 Fix previous migration

--- db/migrate/20170728062206_refactor_spot_optimal_directions.rb
+++ db/migrate/20170728062206_refactor_spot_optimal_directions.rb
@@ -1,7 +1,9 @@
 class RefactorSpotOptimalDirections < ActiveRecord::Migration[5.1]
   def change
-    remove_column :spots, :swell_optimal_direction
-    remove_column :spots, :wind_optimal_direction
+    remove_column :spots, :swell_optimal_direction, :decimal
+    remove_column :spots, :swell_optimal_direction_max_variance, :decimal
+    remove_column :spots, :wind_optimal_direction, :decimal
+    remove_column :spots, :wind_optimal_direction_max_variance, :decimal
     add_column :spots, :swell_optimal_direction_min, :decimal
     add_column :spots, :swell_optimal_direction_max, :decimal
     add_column :spots, :wind_optimal_direction_min, :decimal
