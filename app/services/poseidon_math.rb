80da5cd8b52a36cee24e66b58343f64fe8a3d57c
TaylorBett <taylor.bett@hotmail.com>
2017-08-02 11:31:01 +1000

    Refactor PoseidonMath class to use DRY for obtaining parabola values

--- app/services/poseidon_math.rb
+++ app/services/poseidon_math.rb
@@ -20,36 +20,41 @@ class PoseidonMath
     }
   end
 
-  def rating_given_x(data = {})
-    return 0 unless data
+  def get_h_k_a(data={})
+    return nil unless data
+    maximum_rating = 100.0
+    opt_rating = 75.0
     # use vertex quad formula y = a(x-h)^2 + k
     # a = stretch coefficient, h = x coord of vertex, k = y coord of vertex
-    k_var = 100.0
+    k_var = maximum_rating
     h_var = ((data[:max_x] - data[:min_x]) / 2) + data[:min_x]
+    # pass in known coord to determine var a value, (data[:min_x], opt_rating)
+    a_var = (opt_rating - k_var) / ((data[:min_x] - h_var)**2)
+    {
+      k_var: k_var,
+      h_var: h_var,
+      a_var: a_var
+    }
+  end
 
-    # pass in known coord to determine var a value, (data[:min_x], meta_optimum_x)
-    a_var = (75 - k_var) / ((data[:min_x] - h_var)**2)
-    rating = a_var * ((data[:x_value] - h_var)**2) + k_var
-
+  def rating_given_x(data = {})
+    return 0 unless data
+    vals = get_h_k_a(data)
+    rating = vals[:a_var] * ((data[:x_value] - vals[:h_var])**2) + vals[:k_var]
     rating = 0 if rating.negative?
-
     rating
   end
 
   def value_given_rating(data = {})
     return 0 unless data
-    k_var = 100.0
-    h_var = ((data[:max_x] - data[:min_x]) / 2) + data[:min_x]
+    vals = get_h_k_a(data)
 
-    # pass in known coord to determine var a value, (data[:min_x], meta_optimum_x)
-    a_var = (75 - k_var) / ((data[:min_x] - h_var)**2)
-
-    q_i = 2 * a_var * h_var
-    q_ii = (-2 * a_var * h_var)**2
-    q_iii = 4 * a_var * (a_var * (h_var**2) + k_var - data[:rating])
+    q_i = 2 * vals[:a_var] * vals[:h_var]
+    q_ii = (-2 * vals[:a_var] * vals[:h_var])**2
+    q_iii = 4 * vals[:a_var] * (vals[:a_var] * (vals[:h_var]**2) + vals[:k_var] - data[:rating])
     my_sqrt = q_ii - q_iii
-    s_a_r_right = (q_i - Math.sqrt(my_sqrt.to_f)) / (2 * a_var)
-    s_a_r_left = (q_i + Math.sqrt(my_sqrt.to_f)) / (2 * a_var)
+    s_a_r_right = (q_i - Math.sqrt(my_sqrt.to_f)) / (2 * vals[:a_var])
+    s_a_r_left = (q_i + Math.sqrt(my_sqrt.to_f)) / (2 * vals[:a_var])
     {
       left: s_a_r_left,
       right: s_a_r_right
