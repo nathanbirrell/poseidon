2351a503883090257dabc7cff54c07c654f44765
Nathan Birrell <nathanbirrell@gmail.com>
2017-08-15 17:59:21 +1000

    [cleanup]

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -2,29 +2,33 @@
 #
 # Table name: spots
 #
-#  id                                   :integer          not null, primary key
-#  name                                 :string
-#  description                          :string
-#  season                               :string
-#  created_at                           :datetime         not null
-#  updated_at                           :datetime         not null
-#  latitude                             :decimal(10, 6)
-#  longitude                            :decimal(10, 6)
-#  image                                :string
-#  region_id                            :integer
-#  tide_optimal_min_metres              :decimal(, )
-#  tide_optimal_max_metres              :decimal(, )
-#  swell_optimal_size_min_metres        :decimal(, )
-#  swell_optimal_size_max_metres        :decimal(, )
-#  wind_optimal_strength_min_kmh        :decimal(, )
-#  wind_optimal_strength_max_kmh        :decimal(, )
-#  wave_model_lat                       :decimal(, )
-#  wave_model_lon                       :decimal(, )
-#  willyweather_location_id             :integer
-#  swell_optimal_direction              :decimal(, )
-#  swell_optimal_direction_max_variance :decimal(, )
-#  wind_optimal_direction               :decimal(, )
-#  wind_optimal_direction_max_variance  :decimal(, )
+#  id                            :integer          not null, primary key
+#  name                          :string
+#  description                   :string
+#  season                        :string
+#  created_at                    :datetime         not null
+#  updated_at                    :datetime         not null
+#  latitude                      :decimal(10, 6)
+#  longitude                     :decimal(10, 6)
+#  image                         :string
+#  region_id                     :integer
+#  tide_optimal_min_metres       :decimal(, )
+#  tide_optimal_max_metres       :decimal(, )
+#  swell_optimal_size_min_metres :decimal(, )
+#  swell_optimal_size_max_metres :decimal(, )
+#  wind_optimal_strength_min_kmh :decimal(, )
+#  wind_optimal_strength_max_kmh :decimal(, )
+#  wave_model_lat                :decimal(, )
+#  wave_model_lon                :decimal(, )
+#  willyweather_location_id      :integer
+#  weighting_swell               :decimal(1, 2)
+#  weighting_wind                :decimal(1, 2)
+#  weighting_tide                :decimal(1, 2)
+#  wave_model_size_coefficient   :decimal(1, 3)
+#  swell_optimal_direction_min   :decimal(, )
+#  swell_optimal_direction_max   :decimal(, )
+#  wind_optimal_direction_min    :decimal(, )
+#  wind_optimal_direction_max    :decimal(, )
 #
 
 class Spot < ApplicationRecord
@@ -224,7 +228,7 @@ class Spot < ApplicationRecord
       }
     )
 
-    willyweather_location_id = JSON.parse(response)['location']['id'].to_s
+    self.willyweather_location_id = JSON.parse(response)['location']['id'].to_s
     self.save
   end
 
