413c2370301e14769e5626064af39940ef49cc9a
TaylorBett <taylor.bett@hotmail.com>
2017-08-21 16:13:46 +1000

    F115 | Added core PlaceholderShimmer component and implemented into spot banner region info area

---
+++ app/javascript/placeholder-shimmer.js
@@ -0,0 +1,31 @@
+import React from 'React';
+import PropTypes from 'prop-types';
+
+class PlaceholderShimmer extends React.Component {
+  render() {
+    if (!this.props.width || !this.props.height) {
+      return null;
+    }
+
+    const dimensions = {
+      width: this.props.width,
+      height: this.props.height,
+    }
+
+    return (
+      <div className='placeholder-shimmer' style={dimensions}></div>
+    );
+  }
+}
+
+PlaceholderShimmer.defaultProps = {
+  width: null,
+  height: null,
+};
+
+PlaceholderShimmer.PropTypes = {
+  width: PropTypes.string,
+  height: PropTypes.string,
+};
+
+export default PlaceholderShimmer;
