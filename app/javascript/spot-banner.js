538a5087a11f271105b569f3c096607abfa62004
TaylorBett <taylor.bett@hotmail.com>
2017-08-21 16:36:49 +1000

    F115 | Bulk up the shimmer placeholders in the banner to better suit heading sizing

--- app/javascript/spot-banner.js
+++ app/javascript/spot-banner.js
@@ -13,8 +13,8 @@ class SpotBanner extends React.Component {
             <div className="small-12 columns text-left">
               <div className="spot-banner__rating"></div>
               <div className="spot-banner__details">
-                <PlaceholderShimmer width='180px' height='12px' />
-                <PlaceholderShimmer width='80px' height='8px' />
+                <PlaceholderShimmer width='220px' height='18px' />
+                <PlaceholderShimmer width='100px' height='12px' />
               </div>
             </div>
           </div>
