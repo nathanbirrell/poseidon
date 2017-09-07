f730fd3ff3461327c1f814c9a14a460d5173a6a8
TaylorBett <taylor.bett@hotmail.com>
2017-09-08 09:37:54 +1000

    F129 | Split current/day into separate container to clean up spot container and ready for Time Slider work

--- app/javascript/spot-container.js
+++ app/javascript/spot-container.js
@@ -7,10 +7,10 @@ import SpotUtil from 'spot-util.js';
 
 import SpotBanner from './spot-banner';
 import NavigationTabs from './navigation-tabs';
-import SpotInfoCard from './spot-info-card';
 import AreaGraph from './area-graph';
-import SpotAbout from './spot-about-container';
+import SpotDayContainer from './spot-day-container';
 import SpotForecastContainer from './spot-forecast-container';
+import SpotAboutContainer from './spot-about-container';
 import SpotTimeSlider from './spot-time-slider';
 
 class SpotContainer extends React.Component {
@@ -95,11 +95,7 @@ class SpotContainer extends React.Component {
             items={this.state.navItems}
             onChange={this.updateSelectedNavItem}
           />
-          <div id="current-section" className="row">
-            <SpotInfoCard title='Swell' isBusy />
-            <SpotInfoCard title='Wind' isBusy />
-            <SpotInfoCard title='Tide' isBusy />
-          </div>
+          <SpotDayContainer />
         </div>
       );
     }
@@ -123,204 +119,10 @@ class SpotContainer extends React.Component {
           onChange={this.updateSelectedNavItem}
         />
         {this.state.selectedNavItem === this.state.navItems.indexOf('Today') ?
-          <div id="today-section" className="row">
-            <SpotInfoCard
-              title='Swell'
-              secondary={SpotUtil.swellFeetToDescription(SpotUtil.metresToFeet(current_swell.size))}
-              rating={MathUtil.round(current_swell.rating, 0)}
-              date_time={moment(current_swell.date_time).format("h:mm a")}
-              data={[
-                {
-                  title: 'Rating',
-                  indicator: SpotUtil.getVerdict(current_swell.rating),
-                  prefix: '',
-                  values: [{
-                    value: MathUtil.round(current_swell.rating, 0),
-                    unit: '%',
-                  }],
-                  subtext: `${SpotUtil.getPotential(current_swell.rating)} potential`,
-                },
-                {
-                  title: 'Wave height',
-                  indicator: SpotUtil.getVerdict(current_swell.size_rating),
-                  prefix: '',
-                  values: [{
-                    value: MathUtil.round(SpotUtil.metresToFeet(current_swell.size), 1),
-                    unit: 'ft',
-                  }],
-                  subtext: `@ ${MathUtil.round(current_swell.period, 1)} seconds`,
-                  // optimum_vis: [{
-                  //   label: '',
-                  //   type: 'linear',
-                  //   min: optimals.swell.size.min,
-                  //   max: optimals.swell.size.max,
-                  //   mix_min: optimals.swell.size.mixed_min,
-                  //   mix_max: optimals.swell.size.mixed_max,
-                  //   opt_min: optimals.swell.size.optimal_min,
-                  //   opt_max: optimals.swell.size.optimal_max,
-                  //   value: MathUtil.round(current_swell.size, 1),
-                  //   unit: "m",
-                  //   roc_value: optimals.swell.size.in_3_hours,
-                  //   roc: (optimals.swell.size.in_3_hours - current_swell.size),
-                  //   roc_direction: SpotUtil.getRocDirection(optimals.swell.size.in_3_hours - current_swell.size),
-                  // }]
-                },
-                {
-                  title: 'Direction',
-                  indicator: SpotUtil.getVerdict(current_swell.direction_rating),
-                  prefix: '',
-                  values: [{
-                    value: SpotUtil.degreesToText(current_swell.direction),
-                    unit: '',
-                  }],
-                  subtext: `${current_swell.direction} deg`,
-                  // optimum_vis: [{
-                  //   label: '',
-                  //   type: 'direction',
-                  //   min: optimals.swell.direction.min,
-                  //   max: optimals.swell.direction.max,
-                  //   mix_min: optimals.swell.direction.mixed_min,
-                  //   mix_max: optimals.swell.direction.mixed_max,
-                  //   opt_min: optimals.swell.direction.optimal_min,
-                  //   opt_max: optimals.swell.direction.optimal_max,
-                  //   value: current_swell.direction,
-                  //   unit: "deg",
-                  //   min_label: SpotUtil.degreesToText(optimals.swell.direction.min),
-                  //   max_label: SpotUtil.degreesToText(optimals.swell.direction.max),
-                  //   roc_value: optimals.swell.direction.in_3_hours,
-                  //   roc: (optimals.swell.direction.in_3_hours - current_swell.direction),
-                  //   roc_direction: SpotUtil.getRocDirection(optimals.swell.direction.in_3_hours - current_swell.direction),
-                  // }]
-                }
-              ]}
-            />
-            <SpotInfoCard
-              title='Wind'
-              secondary={SpotUtil.windKphToDescription(current_wind.speed)}
-              rating={MathUtil.round(current_wind.rating, 0)}
-              date_time={moment(current_wind.date_time).format("h:mm a")}
-              data={[
-                {
-                  title: 'Rating',
-                  indicator: SpotUtil.getVerdict(current_wind.rating),
-                  prefix: '',
-                  values: [{
-                    value: MathUtil.round(current_wind.rating, 0),
-                    unit: '%',
-                  }],
-                  subtext: `${SpotUtil.getPotential(current_wind.rating)} potential`,
-                },
-                {
-                  title: 'Wind speed',
-                  indicator: SpotUtil.getVerdict(current_wind.speed_rating),
-                  prefix: '',
-                  values: [{
-                    value: MathUtil.round(SpotUtil.kphToKnots(current_wind.speed), 0),
-                    unit: 'kts',
-                  }],
-                  subtext: `${current_wind.speed} kph`,
-                  // optimum_vis: [{
-                  //   label: '',
-                  //   type: 'linear',
-                  //   min: optimals.wind.speed.min,
-                  //   max: optimals.wind.speed.max,
-                  //   mix_min: optimals.wind.speed.mixed_min,
-                  //   mix_max: optimals.wind.speed.mixed_max,
-                  //   opt_min: optimals.wind.speed.optimal_min,
-                  //   opt_max: optimals.wind.speed.optimal_max,
-                  //   value: current_wind.speed,
-                  //   unit: "kph",
-                  //   roc_value: optimals.wind.speed.in_3_hours,
-                  //   roc: (optimals.wind.speed.in_3_hours - current_wind.speed),
-                  //   roc_direction: SpotUtil.getRocDirection(optimals.wind.speed.in_3_hours - current_wind.speed),
-                  // }]
-                },
-                {
-                  title: 'Direction',
-                  indicator: SpotUtil.getVerdict(current_wind.direction_rating),
-                  prefix: '',
-                  values: [{
-                    value:  SpotUtil.degreesToText(current_wind.direction),
-                    unit: '',
-                  }],
-                  subtext: `${current_wind.direction} deg`,
-                  // optimum_vis: [{
-                  //   label: '',
-                  //   type: 'direction',
-                  //   min: optimals.wind.direction.min,
-                  //   max: optimals.wind.direction.max,
-                  //   mix_min: optimals.wind.direction.mixed_min,
-                  //   mix_max: optimals.wind.direction.mixed_max,
-                  //   opt_min: optimals.wind.direction.optimal_min,
-                  //   opt_max: optimals.wind.direction.optimal_max,
-                  //   value: current_wind.direction,
-                  //   unit: "deg",
-                  //   min_label: SpotUtil.degreesToText(optimals.wind.direction.min),
-                  //   max_label: SpotUtil.degreesToText(optimals.wind.direction.max),
-                  //   roc_value: optimals.wind.direction.in_3_hours,
-                  //   roc: (optimals.wind.direction.in_3_hours - current_wind.direction),
-                  //   roc_direction: SpotUtil.getRocDirection(optimals.wind.direction.in_3_hours - current_wind.direction),
-                  // }]
-                }
-              ]}
-            />
-            <SpotInfoCard
-              title='Tide'
-              secondary={current_tide.state}
-              rating={MathUtil.round(current_tide.rating, 0)}
-              date_time={moment(current_tide.date_time).format("h:mm a")}
-              data={[
-                {
-                  title: 'Rating',
-                  indicator: SpotUtil.getVerdict(current_tide.rating),
-                  prefix: '',
-                  values: [{
-                    value: MathUtil.round(current_tide.rating, 0),
-                    unit: '%',
-                  }],
-                  subtext: `${SpotUtil.getPotential(current_tide.rating)} potential`,
-                },
-                {
-                  title: 'Tide height',
-                  indicator: SpotUtil.getVerdict(current_tide.rating),
-                  prefix: '',
-                  values: [{
-                    value: current_tide.height,
-                    unit: 'm',
-                  }],
-                  subtext: `${current_tide.shift_rate} shift`,
-                  // optimum_vis: [{
-                  //   label: '',
-                  //   type: 'linear',
-                  //   min: optimals.tide.height.min,
-                  //   max: optimals.tide.height.max,
-                  //   mix_min: optimals.tide.height.mixed_min,
-                  //   mix_max: optimals.tide.height.mixed_max,
-                  //   opt_min: optimals.tide.height.optimal_min,
-                  //   opt_max: optimals.tide.height.optimal_max,
-                  //   value: this.state.data.current_tide_snapshot.height,
-                  //   unit: "m",
-                  //   roc_value: optimals.tide.height.in_3_hours,
-                  //   roc: (optimals.tide.height.in_3_hours - this.state.data.current_tide_snapshot.height),
-                  //   roc_direction: SpotUtil.getRocDirection(optimals.tide.height.in_3_hours - this.state.data.current_tide_snapshot.height),
-                  // }]
-                },
-                {
-                  title: 'Next tide',
-                  indicator: '',
-                  prefix: '',
-                  values: [{
-                    value: moment(this.state.forecasts.tides[this.state.selectedTime + 1].date_time).format("h:mm"),
-                    unit: moment(this.state.forecasts.tides[this.state.selectedTime + 1].date_time).format("a"),
-                  }],
-                  subtext: moment(this.state.forecasts.tides[this.state.selectedTime + 1].date_time).fromNow(),
-                  // optimum_vis: [
-                  //
-                  // ]
-                }
-              ]}
-            />
-          </div>
+          <SpotDayContainer
+            selectedTime={this.state.selectedTime}
+            forecasts={this.state.forecasts}
+          />
         : null}
         {this.state.selectedNavItem === this.state.navItems.indexOf('Forecast') ?
           <SpotForecastContainer
@@ -328,7 +130,7 @@ class SpotContainer extends React.Component {
           />
         : null}
         {this.state.selectedNavItem === this.state.navItems.indexOf('About') ?
-          <SpotAbout
+          <SpotAboutContainer
            data={this.state.spot}
           />
         : null}
