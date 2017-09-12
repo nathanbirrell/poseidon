db3dda04ffd6c8fdbc7ebe784229a7b3473b41d9
Nathan Birrell <nathanbirrell@gmail.com>
2017-09-13 08:17:25 +1000

    Finalise markup structure for spot tile

--- app/javascript/components/SpotTile.js
+++ app/javascript/components/SpotTile.js
@@ -1,38 +1,61 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
-import SpotUtil from 'lib/SpotUtil'
+import MathUtil from 'lib/MathUtil';
+import SpotUtil from 'lib/SpotUtil';
 import { Link } from 'react-router-dom';
 import Row from 'components/Row';
 import Column from 'components/Column';
+
+const SpotTileCondition = (props) => {
+  return (
+    <div className="spot-tile__condition">
+      <span className="spot-tile__condition-primary">{props.primary}</span>
+      <span className="spot-tile__condition-secondary">{props.secondary}</span>
+    </div>
+  );
+};
 
 class SpotTile extends React.Component {
   render() {
     const { spot } = this.props;
     const link = `/spots/${spot.id}`;
+    const updated_at = moment(spot.current_model_date_time).fromNow();
+    const swell_size_ft = MathUtil.round(SpotUtil.metresToFeet(spot.current_swell.size), 1);
+    const swell_direction = SpotUtil.degreesToText(spot.current_swell.direction);
+    const swell_period = MathUtil.round(spot.current_swell.period, 0);
+    const wind_direction = SpotUtil.degreesToText(spot.current_wind.direction);
     return (
       <Row>
-        <Column className="spot-tile">
-          <Link to={link} >
+        <Link to={link} className="spot-tile" >
 
-            <div className="spot-tile__rating">
-              {spot.current_potential}
+          <div className="spot-tile__rating">
+            {spot.current_potential}
+          </div>
+          <div className="spot-tile__body">
+            <div className="spot-tile__name">
+              <h3>{spot.name}</h3>
+              <p>{spot.region.name}</p>
             </div>
-            <div className="spot-tile__body">
-              {spot.name}
-              {spot.region.name}
-              {spot.current_swell.size}
-              {spot.current_swell.direction}
-              {spot.current_swell.period}
-              {spot.current_wind.direction}
-              {spot.current_tide_snapshot.type} tide
-              {spot.current_tide_snapshot.height}
+            <div className="spot-tile__conditions">
+              <SpotTileCondition
+                primary={`${swell_size_ft} ft`}
+                secondary={`${swell_direction} @ ${swell_period}s`}
+              />
+              <SpotTileCondition
+                primary={`${wind_direction}`}
+                secondary={`MODERATE TODO`}
+              />
+              <SpotTileCondition
+                primary={`${spot.current_tide_snapshot.type} tide`}
+                secondary={`${spot.current_tide_snapshot.height}`}
+              />
             </div>
-            <div className="spot-tile__updated">
-              UPDATED {spot.current_model_date_time}
-            </div>
-          </Link>
-        </Column>
+          </div>
+          <div className="spot-tile__updated">
+            {updated_at}
+          </div>
+        </Link>
       </Row>
     );
   }
