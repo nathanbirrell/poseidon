52356ab608f42053fb57287d0e6c3de359c691e7
TaylorBett <taylor.bett@hotmail.com>
2017-10-05 12:47:10 +1100

    Added region filter to spots list page

--- app/javascript/containers/SpotsListContainer.js
+++ app/javascript/containers/SpotsListContainer.js
@@ -16,6 +16,7 @@ class SpotsListContainer extends React.Component {
     };
 
     this.listSpots = this.listSpots.bind(this);
+    this.handleRegionChange = this.handleRegionChange.bind(this);
   }
 
   componentDidMount() {
@@ -30,7 +31,12 @@ class SpotsListContainer extends React.Component {
   listSpots() {
     if (!this.state.spots) return null;
 
-    return this.state.spots.map(spot => {
+    let filteredSpots = this.state.selectedRegion ?
+      this.state.spots.filter(spot => {
+        return spot.region_id == this.state.selectedRegion
+      }) : this.state.spots;
+
+    return filteredSpots.map(spot => {
       return (<SpotTile spot={spot} key={spot.id} />);
     });
   }
@@ -41,11 +47,26 @@ class SpotsListContainer extends React.Component {
     );
   }
 
+  handleRegionChange(event) {
+    this.setState({
+      selectedRegion: event.target.value
+    });
+  }
+
   render() {
     return (
       <Row>
         <Column widthMedium={12} widthLarge={10} isCentered>
           <h1>Surf now</h1>
+          <select
+            className="filter-select"
+            onChange={this.handleRegionChange}
+            value={this.state.regionValue}
+          >
+            <option value="">All regions</option>
+            <option value="1">Mornington Peninsula</option>
+            <option value="2">Surf Coast</option>
+          </select>
         </Column>
 
         <Column className="spots-list small-expanded" widthMedium={12} widthLarge={10} isCentered>
