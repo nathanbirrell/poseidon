b7e7053115c36f8d1d03214a6293303027ef75cb
Nathan Birrell <nathanbirrell@gmail.com>
2017-10-10 22:57:15 +1100

    Get footer in shape

--- app/javascript/components/Footer.js
+++ app/javascript/components/Footer.js
@@ -1,12 +1,25 @@
 import React from 'react';
-
+import Row from 'components/Row';
 class Footer extends React.Component {
   render() {
     return (
-      <footer className="">
-        <div className="">
-          TODO
-        </div>
+      <footer className="footer">
+        <Row withXPadding withYPadding>
+          <div className="medium-6 cell">
+            <ul className="footer__links">
+              <li><a href="#">Home</a></li>
+              <li><a href="#">Account</a></li>
+              <li><a href="#">About</a></li>
+            </ul>
+          </div>
+
+          <div className="medium-6 cell text-right">
+            <p>
+              Built with 🤙🏼  in Melbourne. <br />
+              Copyright Surf Poseidon Pty Ltd 2017
+            </p>
+          </div>
+        </Row>
       </footer>
     );
   }
--- app/javascript/components/Row.js
+++ app/javascript/components/Row.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Classnames from 'classnames';
 
 const Row = (props) => {
-  const { children } = props;
+  const { children, withXPadding, withYPadding } = props;
+
+  const classes = Classnames({
+    'grid-x': true,
+    'grid-padding-x': withXPadding,
+    'grid-padding-y': withYPadding,
+  });
   return (
-    <div className="grid-x">
+    <div className={classes}>
       {children}
     </div>
   );
@@ -12,10 +19,14 @@ const Row = (props) => {
 
 Row.defaultProps = {
   children: null,
+  withXPadding: false,
+  withYPadding: false,
 };
 
 Row.PropTypes = {
   children: PropTypes.node,
+  withXPadding: PropTypes.bool,
+  withYPadding: PropTypes.bool,
 };
 
 export default Row;
