20207058c69e91a8efb063676ca51c594fd1034c
Nathan Birrell <nathanbirrell@gmail.com>
2017-10-10 22:57:15 +1100

    Remove image processing until we have time to set it up properly

--- config/webpack/loaders/images.js
+++ config/webpack/loaders/images.js
@@ -1,12 +1,12 @@
-module.exports = {
-  test: /\.(gif|png|jpe?g|svg)$/i,
-  use: [
-    'file-loader',
-    {
-      loader: 'image-webpack-loader',
-      options: {
-        // bypassOnDebug: true, // Use to speed up initial/subsequent dev build times
-      },
-    },
-  ],
-}
+// module.exports = {
+//   test: /\.(gif|png|jpe?g|svg)$/i,
+//   use: [
+//     'file-loader',
+//     {
+//       loader: 'image-webpack-loader',
+//       options: {
+//         bypassOnDebug: true, // Use to speed up initial/subsequent dev build times
+//       },
+//     },
+//   ],
+// }
