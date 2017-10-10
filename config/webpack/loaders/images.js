b70d52425875623f1e5128820fdea53bd5f0778f
Nathan Birrell <nathanbirrell@gmail.com>
2017-10-10 22:57:15 +1100

    Move images and icons into webpack build, add a loader for them

---
+++ config/webpack/loaders/images.js
@@ -0,0 +1,12 @@
+module.exports = {
+  test: /\.(gif|png|jpe?g|svg)$/i,
+  use: [
+    'file-loader',
+    {
+      loader: 'image-webpack-loader',
+      options: {
+        // bypassOnDebug: true, // Use to speed up initial/subsequent dev build times
+      },
+    },
+  ],
+}
