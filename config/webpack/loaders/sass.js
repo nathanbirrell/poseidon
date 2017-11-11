1dde20227d3ffad607442fd0dc0538515fdd5e51
Nathan Birrell <nathanbirrell@gmail.com>
2017-11-11 13:51:00 +1100

    Fix sass sourcemaps

--- config/webpack/loaders/sass.js
+++ config/webpack/loaders/sass.js
@@ -6,7 +6,7 @@ module.exports = {
   use: ExtractTextPlugin.extract({
     fallback: 'style-loader',
     use: [
-      { loader: 'css-loader', options: { minimize: env.NODE_ENV === 'production' } },
+      { loader: 'css-loader', options: { minimize: env.NODE_ENV === 'production', sourceMap: true } },
       { loader: 'postcss-loader', options: { sourceMap: true } },
       'resolve-url-loader',
       {
