5a9c61b6bfccdfa15bd8c1d0406ba31aca1a1c5d
Nathan Birrell <nathanbirrell@gmail.com>
2017-11-20 15:20:54 +1100

    F164 Content

--- app/javascript/pages/MarketingPage.js
+++ app/javascript/pages/MarketingPage.js
@@ -4,31 +4,38 @@ import Row from 'components/Row';
 import Column from 'components/Column';
 import Button from 'components/Button';
 
+import heroImg from 'images/marketing/hero.png';
+
 class HomePage extends React.Component {
   render() {
+    console.log(heroImg);
     return (
       <div className="marketing-page">
-        <div className="callout large">
-          <div className="row column text-center">
-            <h1>Changing the World Through Design</h1>
-            <p className="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in dui mauris.</p>
-            <a href="#" className="button large">Learn More</a>
-            <Button>Learn more for real</Button>
-            <a href="#" className="button large hollow">Learn Less</a>
-          </div>
-        </div>
-        <Row withYPadding>
-          <Column isAutoMediumUp>
-            <h3>Photoshop</h3>
-            <p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.</p>
+        <Row className="hero-image callout large" style={{ backgroundImage: `url(${heroImg})` }}>
+          <Column widthMedium={8}>
+            <h1>Intelligent and reliable surf forecasting built for you.</h1>
+            <p className="lead">
+              Surf like a God with dead-simple surf forecasts, spot recommendations and Australia's most reliable weather modelling.
+            </p>
+            <Button>JOIN THE WAITLIST</Button>
           </Column>
-          <Column isAutoMediumUp>
-            <h3>Javascript</h3>
-            <p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.</p>
-          </Column>
-          <Column isAutoMediumUp>
-            <h3>Marketing</h3>
-            <p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.</p>
+        </Row>
+        <Row className="cvp-container" withYPadding withXPadding>
+          <Column widthMedium={10} offsetMedium={1} widthLarge={10} offsetLarge={1}>
+            <Row>
+              <Column isAutoMediumUp>
+                <h3>Australia's top modelling</h3>
+                <p>Powered by BOM and the NOAA, you can now access all of surfing's most reliable sources in one place. TODO ADD BOM/NOAA IMAGES FOR LEGITNESS</p>
+              </Column>
+              <Column isAutoMediumUp>
+                <h3>Plan less, <br />surf more</h3>
+                <p>Poseidon's proprietary rating algorithm tells you where's pumping, so you can forget about ideal swells, winds and tides and spend more time in the water.</p>
+              </Column>
+              <Column isAutoMediumUp>
+                <h3>Mobile first, notifications m8</h3>
+                <p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.</p>
+              </Column>
+            </Row>
           </Column>
         </Row>
       </div>
