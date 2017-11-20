fc497bb0f791ac48f5f6deb43e1028086c069296
Nathan Birrell <nathanbirrell@gmail.com>
2017-11-20 13:36:03 +1100

    Start template for simple marketing site

--- app/javascript/pages/MarketingPage.js
+++ app/javascript/pages/MarketingPage.js
@@ -1,10 +1,34 @@
 import React from 'react';
 
+import Row from 'components/Row';
+import Column from 'components/Column';
+
 class HomePage extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Poseidon</h1>
+      <div className="marketing-page">
+        <div className="callout large">
+          <div className="row column text-center">
+            <h1>Changing the World Through Design</h1>
+            <p className="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in dui mauris.</p>
+            <a href="#" className="button large">Learn More</a>
+            <a href="#" className="button large hollow">Learn Less</a>
+          </div>
+        </div>
+        <Row withYPadding>
+          <Column isAutoMediumUp>
+            <h3>Photoshop</h3>
+            <p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.</p>
+          </Column>
+          <Column isAutoMediumUp>
+            <h3>Javascript</h3>
+            <p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.</p>
+          </Column>
+          <Column isAutoMediumUp>
+            <h3>Marketing</h3>
+            <p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.</p>
+          </Column>
+        </Row>
       </div>
     );
   }
