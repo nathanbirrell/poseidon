5a9cdb710becc83e5b748282a6d4f76e6f127464
Nathan Birrell <nathanbirrell@gmail.com>
2017-12-04 13:30:26 +1100

    F81 Retrieve weather day summary data

---
+++ lib/willyweather_client/weather_day_summary_forecasts.rb
@@ -0,0 +1,42 @@
+module WillyweatherClient
+  class TideForecasts
+    attr_reader :location, :forecasts
+
+    def initialize(response, spot, client)
+      @spot = spot
+      @location = response['location']
+      @forecasts = response['forecasts']['weather']['days']
+      @client = client
+    end
+
+    def self.fetch(spot)
+      client = WillyweatherClient::Client.new(spot.willyweather_location_id)
+      response = client.fetch_forecasts('weather')
+      new(response, spot, client)
+    end
+
+    def save_entries
+      @forecasts.each do |day|
+        day['entries'].each do |forecast|
+          save_record(forecast)
+        end
+      end
+    end
+
+    def save_record(forecast)
+      record = @client.get_or_create_record(
+        WeatherDaySummary,
+        @location['timeZone'],
+        forecast['dateTime'],
+        @spot.id
+      )
+
+      record.temp_min = forecast['min']
+      record.temp_max = forecast['max']
+      record.precis_code = forecast['precisCode']
+      record.precis = forecast['precis']
+      record.precis_overlay_code = forecast['precisOverlayCode']
+      record.save
+    end
+  end
+end
