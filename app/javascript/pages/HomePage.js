3c0fd2b0680c09f1a4082ce9c1129f01b70aaec1
Nathan Birrell <nathanbirrell@gmail.com>
2018-01-31 17:30:41 +1100

    WIP building out a toggle element

--- app/javascript/pages/HomePage.js
+++ app/javascript/pages/HomePage.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import SpotsListContainer from 'containers/SpotsListContainer';
+import Toggle from 'components/Toggle';
 
 class HomePage extends React.Component {
   render() {
     return (
-      <SpotsListContainer />
+      <div>
+        <Toggle
+          id="asdf"
+          labelA="asdf"
+          labelB="asdf"
+        />
+        <SpotsListContainer />
+      </div>
     );
   }
 }
