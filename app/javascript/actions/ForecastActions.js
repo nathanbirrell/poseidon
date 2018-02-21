76ef2fbfee610b95ee9302bb130f618dc5c9ba68
Nathan Birrell <nathanbirrell@gmail.com>
2018-02-21 19:24:27 +1100

    Refactor actions for forecasts and spot

--- app/javascript/actions/ForecastActions.js
+++ app/javascript/actions/ForecastActions.js
@@ -1,37 +1,16 @@
+import * as ReduxUtils from 'lib/ReduxUtils';
 import moment from 'moment';
 import SpotService from 'services/SpotService';
 
 import { FETCH_SURF_FORECAST, CHANGE_SELECTED_DATETIME } from 'types';
-
-export const fetchSurfForecastRequest = () => ({
-  type: FETCH_SURF_FORECAST.REQUEST,
-});
-
-export const fetchSurfForecastSuccess = (data) => ({
-  type: FETCH_SURF_FORECAST.SUCCESS,
-  data,
-});
-
-export const fetchSurfForecastError = (error) => ({
-  type: FETCH_SURF_FORECAST.ERROR,
-  error,
-});
 
 export const updateSelectedDateTime = (selectedDateTime) => ({
   type: CHANGE_SELECTED_DATETIME,
   selectedDateTime: moment(selectedDateTime),
 });
 
-export const fetchSurfForecast = (spotId) => {
-  return async function (dispatch) {
-    dispatch(fetchSurfForecastRequest());
-
-    try {
-      const forecast = await SpotService.fetchSurfForecast(spotId);
-      dispatch(fetchSurfForecastSuccess(forecast));
-    } catch (error) {
-      dispatch(fetchSurfForecastError(error));
-      console.error(error);
-    }
-  };
-};
+export const fetchSurfForecast = (spotId) => (dispatch) => ReduxUtils.apiSyncAction(
+  dispatch,
+  FETCH_SURF_FORECAST,
+  SpotService.fetchSurfForecast.bind(null, spotId),
+);
--- app/javascript/actions/SpotActions.js
+++ app/javascript/actions/SpotActions.js
@@ -1,31 +1,9 @@
+import * as ReduxUtils from 'lib/ReduxUtils';
 import SpotService from 'services/SpotService';
-
 import { FETCH_SPOT } from 'types';
 
-export const fetchSpotRequest = () => ({
-  type: FETCH_SPOT.REQUEST,
-});
-
-export const fetchSpotSuccess = (data) => ({
-  type: FETCH_SPOT.SUCCESS,
-  data,
-});
-
-export const fetchSpotError = (error) => ({
-  type: FETCH_SPOT.ERROR,
-  error,
-});
-
-export const fetchSpot = (spotId) => {
-  return async function (dispatch) {
-    dispatch(fetchSpotRequest());
-
-    try {
-      const spot = await SpotService.fetchSpot(spotId);
-      dispatch(fetchSpotSuccess(spot));
-    } catch (error) {
-      dispatch(fetchSpotError(error));
-      console.error(error);
-    }
-  };
-};
+export const fetchSpot = (spotId) => (dispatch) => ReduxUtils.apiSyncAction(
+  dispatch,
+  FETCH_SPOT,
+  SpotService.fetchSpot.bind(null, spotId),
+);
