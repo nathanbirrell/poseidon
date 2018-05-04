284b8f9fe4c585b16d7490f7f34a65b629ae1a1f
Nathan Birrell <nathanbirrell@gmail.com>
2018-05-04 18:08:27 +1000

    Fix prod Safari

--- app/javascript/store.js
+++ app/javascript/store.js
@@ -3,14 +3,26 @@ import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
 export default function configureStore(initialState) {
-  const store = createStore(
-    rootReducer,
-    initialState,
-    compose(
-      applyMiddleware(thunk),
-      window.devToolsExtension ? window.devToolsExtension() : undefined,
-    ),
-  );
+  let store;
+
+  // Only chrome can handle the redux dev tool
+  // redux compose cannot handle a null or undefined middleware
+  if (window.navigator.userAgent.includes('Chrome')) {
+    store = createStore(
+      rootReducer,
+      initialState,
+      compose(
+        applyMiddleware(thunk),
+        window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : undefined,
+      ),
+    );
+  } else {
+    store = createStore(
+      rootReducer,
+      initialState,
+      compose(applyMiddleware(thunk)),
+    );
+  }
 
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
